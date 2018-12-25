1285de036038711a97afb3f4a08495adc95bc787
Sushanth <sushanthmukkamalla@gmail.com>
2018-12-25 00:00:34 -0800

    some changes

--- youtube_api_app/main.js
+++ youtube_api_app/main.js
@@ -1,8 +1,6 @@
 // Options
 const CLIENT_ID = '948908604760-qupiruja1rjo0gokbmeo5cnmid3rup8l.apps.googleusercontent.com';
-const DISCOVERY_DOCS = [
-  'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
-];
+const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
 const authorizeButton = document.getElementById('authorize-button');
